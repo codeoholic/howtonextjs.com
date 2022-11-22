0797280aa91018384dc84692794eff4f05e78853
Mohit Tyagi <mohit@hey.com>
2022-11-23 00:02:58 +0530

    feat: icons in footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 const { version } = require("../../package.json")
 
 const Footer = () => {
@@ -5,7 +7,19 @@ const Footer = () => {
     return (
 
         <div className="container max-w-4xl mx-auto py-5">
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex gap-5 justify-center items-center">
+                <div className="h-8 w-8 relative">
+                    <Link href="https://twitter.com/howtonextjs" passHref target="_blank">
+                        <img src="/twitter.svg" alt="twitter_icon"/>
+                    </Link>
+                </div>
+                <div className="h-8 w-8 relative">
+                    <Link href="https://t.me/howtonextjs" passHref target="_blank">
+                        <img src="/telegram.svg" alt="telegram_icon"/>
+                    </Link>
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center mt-5">
                 <p className="text-xs text-slate-500">v{ version }</p>
                 <div className="mt-1">
                     <p className="text-[8px] text-slate-500 uppercase">in technology we trust</p>
