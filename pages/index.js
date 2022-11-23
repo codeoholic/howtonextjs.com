a6402a6aeb26c34ff12de3a460b8b0cfdff12d96
Mohit Tyagi <mohit@hey.com>
2022-11-23 12:55:26 +0530

    feat: slug page

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,14 @@ const Home = () => {
 					<h2 className="text-sm md:text-xl text-indigo-50">Simply the most convient blog to start your NextJS career.</h2>
 				</div>
 				<Link href="https://twitter.com/howtonextjs" passHref target="_blank">
+					<a>
 					<div className="rounded-full bg-white px-5 py-2.5 mt-5 flex gap-2.5 items-center shadow-md shadow-indigo-700">
 						<div className="h-4 w-4 relative">
 							<img src="/twitter.svg" alt="twitter_icon"/>
 						</div>
 						<p className="text-xs md:text-sm text-zinc-800">Any questions? Ask me.</p>
 					</div>
+					</a>
 				</Link>
 			</div>
 			<div className="container mx-auto py-5 px-5 md:px-0">
