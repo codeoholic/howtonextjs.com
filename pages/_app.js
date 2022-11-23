0570a9e02739d7ca7ddd85a4c166cc970b78689e
Mohit Tyagi <mohit@hey.com>
2022-11-23 11:56:02 +0530

    feat: fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+	const router = useRouter()
+	useEffect(() => {
+
+		Fathom.load('GEEZGXKM', {
+		  
+			includedDomains: ['howtonextjs.com'],
+			url:"https://bank.howtonextjs.com/script.js",
+
+		})
+	
+		function onRouteChangeComplete() {
+		  	
+			Fathom.trackPageview()
+
+		}
+		router.events.on('routeChangeComplete', onRouteChangeComplete)
+		return () => {
+		  
+			router.events.off('routeChangeComplete', onRouteChangeComplete)
+
+		}
+
+	}, [])
+  
+	return <Component {...pageProps} />
+
 }
 
 export default MyApp
